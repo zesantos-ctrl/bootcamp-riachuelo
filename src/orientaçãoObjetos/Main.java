83a1d9cdb43cc22926c3ec5e85e54d91b96cf9d1
zé <jvitinhosa@gmail.com>
2025-08-22 11:40:20 -0300

    commit

--- src/orientaçãoObjetos/Main.java
+++ src/orientaçãoObjetos/Main.java
@@ -1,5 +1,15 @@
 package orientaçãoObjetos;
 
 public class Main {
+    public static void main(String[] args) {
+        Person male = new Person();
+        male.setName("José");
+        male.setAge(21);
+        Person famale = new Person();
+        famale.setName("gabi");
+        famale.setAge(20);
+        System.out.println("Male name: " + male.getName() +  " age: " + male.getAge());
+        System.out.println("Famale name: " + famale.getName() + " age: " + famale.getAge());
 
+    }
 }
---
+++ src/orientaçãoObjetos/Person.java
@@ -0,0 +1,23 @@
+package orientaçãoObjetos;
+
+public class Person {
+    private final String name;
+    private int age;
+
+    public Person() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
